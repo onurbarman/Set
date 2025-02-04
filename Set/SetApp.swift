2baf34afc4090afe9589af7c6971b9df9c5ddf71
Onur Barman <onur.barman@scalefocus.com>
2025-02-04 17:06:30 +0300

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by MacOS on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SetTests/SetTests.swift
@@ -0,0 +1,17 @@
+//
+//  SetTests.swift
+//  SetTests
+//
+//  Created by MacOS on 2/4/25.
+//
+
+import Testing
+@testable import Set
+
+struct SetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
